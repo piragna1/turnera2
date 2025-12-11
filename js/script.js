ffd7266f978dba43d1dc7a90864d2854555bb474
piragna <gonzalovarelaalagna@gmail.com>
2025-12-11 07:28:03 -0300

    Despersistencia de turnos antes de agregar nuevo

--- js/script.js
+++ js/script.js
@@ -1,27 +1,42 @@
-turnos = [];
+let turnos = [];
 
 function agregarTurno(event) {
   event.preventDefault();
+
+  //validacion
+  let formularioValido = validarTurno();
+  if (!formularioValido) return;
+
+  //obtencion de valores
   let nombre = document.getElementById("nombre").value;
   let apellido = document.getElementById("apellido").value;
   let telefono = document.getElementById("telefono").value;
   let fecha = document.getElementById("fecha").value;
   let hora = document.getElementById("hora").value;
-  let formularioValido = validarTurno();
-  if (formularioValido) {
-    turnos.push({
-      nombre,
-      apellido,
-      telefono,
-      fecha,
-      hora,
-    });
-    console.log(turnos[turnos.length - 1]);
-    form.reset();
 
-    // Store object
-    localStorage.setItem("turnos", JSON.stringify(turnos));
+  //objeto turno
+  let turno = {
+    nombre,apellido,telefono,fecha,hora
   }
+
+  //reset del formulario
+  form.reset();
+  
+  
+  //despersistencia de los turnos que estan en localstorage
+  turnos = localStorage.getItem('turnos');
+
+  //ya existen turnos
+  if (turnos){
+    const parsedTurnos = JSON.parse(turnos);
+    turnos = parsedTurnos;
+  }
+
+  //agrego el nuevo turno
+  turnos.push(turno);
+  
+  //Guardo la lista de turnos nueva en localstorage
+  localStorage.setItem("turnos", JSON.stringify(turnos));
 }
 
 function validarTurno() {
