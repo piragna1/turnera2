f3d84f9f493e35fbf08b380a012bbaa546767d78
piragna <gonzalovarelaalagna@gmail.com>
2025-12-11 07:27:39 -0300

    carga de turnos en pantalla

--- js/lista-turnos.js
+++ js/lista-turnos.js
@@ -5,6 +5,27 @@ function listarTurnos(){
     if (turnos){
         const parsedTurnos = JSON.parse(turnos);
         console.log(parsedTurnos);
+        //selecciono la lista por id
+        const lista = document.getElementById('lista-turnos');
+
+        parsedTurnos.forEach(element => {
+            //creo un elemento li
+            const nuevoElemento = document.createElement('li');
+            //creo el nodo con el texto
+            const nodo = document.createTextNode(
+                element.nombre + ' - ' +
+                element.apellido + ' - ' +
+                element.telefono + ' - ' + 
+                element.fecha + ' - ' + 
+                element.hora
+                // 'hola'
+            );
+            //agrego el texto al li
+            nuevoElemento.appendChild(nodo);
+            //agrego el li a la lista
+            lista.appendChild(nuevoElemento);
+        });
+
     }
     else {
         console.log('Aun no hay turnos cargados...')
